16986619259891d4e7130a3556e7d979ad935a51
Yu ZHANG <yves.zhang@gmail.com>
2012-09-19 22:16:56 +0800

    modified:   README 	deleted:    test/ConcurrentLinkedQueue-1347441364543.testlog 	deleted:    test/ConcurrentLinkedQueue-1347441920502.testlog 	modified:   veritrace.py

--- veritrace.py
+++ veritrace.py
@@ -2,9 +2,10 @@
 
 import sys
 import subprocess 
+import os
 
 class TestCase :
-    def __init__(self, clspath, clsname, mthds, thdNum, evtNum, prog, outf) :
+    def __init__(self, clspath, clsname, mthds, thdNum, evtNum, prog, outf, verb, repeat) :
         self.importPath = clspath
         self.classname = clsname
         self.methods = mthds
@@ -12,6 +13,8 @@ class TestCase :
         self.traceLength = evtNum 
         self.testProgram = prog
         self.outFile = outf
+        self.verbose = verb
+        self.repeat = repeat
 
     def __repr__(self) :
         if len(self.methods) <= 0 :
@@ -23,7 +26,7 @@ class TestCase :
         return "Testing " + self.classname + " (" + str + ") with " + repr(self.threadNum) + " threads." 
 
 def parseCommandLine() :
-    usage = "Usage: veritrace.py [-import <import path>] [-thread <thread number>] [-trace <trace length>] [-output <output file>] <class name> <method name 1> ... <method name n>" 
+    usage = "Usage: veritrace.py [-verbose] [-repeat <test number>] [-import <import path>] [-thread <thread number>] [-trace <trace length>] [-output <output file>] <class name> <method name 1> ... <method name n>" 
     if len(sys.argv) < 2 :
         print usage
         exit(0)
@@ -37,6 +40,8 @@ def parseCommandLine() :
         # tag = 0: class name; 1: method name; 2: import path; 3: thread number; 4: trace length; 5: output 
         tag = 0
         gotClass = False
+        verb = False
+        repeat = 0
         for s in sys.argv[1:] :
             if s[0] == "-" :
                 if s[1:] == "import" :
@@ -47,6 +52,14 @@ def parseCommandLine() :
                     tag = 4
                 elif s[1:] == "output" :
                     tag = 5
+                elif s[1:] == "repeat" :
+                    tag = 6
+                elif s[1:] == "verbose" :
+                    verb = True 
+                    if gotClass : 
+                        tag = 1
+                    else :
+                        tag = 0
             else: 
                 if tag == 0 :
                     clsname = s 
@@ -82,6 +95,12 @@ def parseCommandLine() :
                         tag = 1
                     else :
                         tag = 0
+                elif tag == 6 : 
+                    repeat = int(s) 
+                    if gotClass : 
+                        tag = 1
+                    else :
+                        tag = 0
 
         if clsname == "" :
             print "Error: no test class is given!" 
@@ -90,24 +109,49 @@ def parseCommandLine() :
             print "Error: no test method is given!" 
             print usage 
         prog = "testing" + clsname + ".java"
-        return TestCase(path, clsname, mthds, thdNum, trLeng, prog, outf)
+        return TestCase(path, clsname, mthds, thdNum, trLeng, prog, outf, verb, repeat)
+
+vtHomePath = os.environ['VT_HOME'].rstrip("/ ")
+if vtHomePath == "" :
+    print "Please set the VT_HOME environment variable!" 
+    exit (0) 
+
+agentPath = vtHomePath + "/jvmagent/TraceAgent.dylib"
+testPath = vtHomePath + "/test"
+scalaPath = vtHomePath + "/serialize"
+logPath = vtHomePath + "/tracelog"
 
 test = parseCommandLine ()
 
-agentPath = "/Users/zhang/mycode/VeriTrace/jvmagent/TraceAgent.dylib"
-testPath = "/Users/zhang/mycode/VeriTrace/test"
 testProgram = "Testing" + test.classname 
-if test.outFile == "" :
-    agentArgs = "=" 
-    testArgs = ""
-else :
-    agentArgs = "=" + test.outFile + "," 
-    testArgs = "-" + test.outFile + " " 
-agentArgs = agentArgs + repr(test.threadNum) + "," + repr(test.traceLength) + "," + test.classname 
-testArgs = testArgs + repr(test.threadNum) + " " + repr(test.traceLength) + " " + test.classname 
-for methodName in test.methods:
-    agentArgs = agentArgs + "," + methodName 
-    testArgs = testArgs + " " + methodName 
-testCommand = "java -agentpath:" + agentPath + agentArgs + " -classpath " + testPath + " " + testProgram + " " + testArgs 
-print testCommand
-subprocess.call(testCommand, shell=True)
+outNames = map((lambda x: test.outFile + "_%d_%d_%05d" % (test.threadNum, test.traceLength, x)), range(0, test.repeat))
+for name in outNames:
+    if test.outFile == "" :
+        agentArgs = "=" 
+        testArgs = ""
+    else :
+        agentArgs = "=" + logPath + "/" + name + ","  
+        testArgs = "-" + logPath + "/" + name + " "
+    agentArgs = agentArgs + repr(test.threadNum) + "," + repr(test.traceLength) + "," + test.classname 
+    testArgs = testArgs + repr(test.threadNum) + " " + repr(test.traceLength) + " " + test.classname 
+    for methodName in test.methods:
+        agentArgs = agentArgs + "," + methodName 
+        testArgs = testArgs + " " + methodName 
+    testCommand = "java -agentpath:" + agentPath + agentArgs + " -classpath " + testPath + " " + testProgram + " " + testArgs 
+    # print testCommand
+    subprocess.call(testCommand, shell=True)
+
+
+if test.outFile != "" :
+    os.chdir(scalaPath) 
+    if test.verbose :
+        verifCommand = "scala Serialize -v " + logPath + "/" 
+    else : 
+        verifCommand = "scala Serialize " + logPath + "/" 
+    caseNo = 1
+    for name in outNames: 
+        print ("Test case #" + str(caseNo))
+        subprocess.call(verifCommand+name, shell=True)
+        caseNo += 1
+    # for name in outNames: 
+    #     subprocess.call("rm -f " + testPath
